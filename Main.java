e6f41ea5af0bbfe519e2b22b07ca6efce60cd12e
Alpha <dalpha16@gmail.com>
2023-05-11 15:28:04 +0200

    1er commit du main.java et de la classe personne

---
+++ Main.java
@@ -0,0 +1,69 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /* lorsque l'on declare une variable de type objet , on dit que l'on fait une INSTANCIATION .
+        un objet est une INSTANCE d'une  classe .
+        @return String concaténation du prénom et du nom 
+        @var aucune variable n'est déclaré dans cette méthode 
+          */
+      Personne p = new Personne("Onyme","Anne",50) ;
+      p.prenom = " Anne ";
+      p.nom = "Onyme"; 
+      p.setAge(50) ; 
+      System.out.println("Nom : " + p.nom);
+      System.out.println("Prénom :" + p.prenom);
+      System.out.println("Age : " + p.getAge());
+      
+
+      System.out.println(p.identite());
+
+      Personne p1 = new Personne("jean ","Cérien"); 
+    //   p1.prenom = "Jean";
+    //   p1.nom = "Cerien";
+      p1.setAge(25); 
+      System.out.println("Identité de p1 : " + p1.identite());
+
+
+      /* 1. déclarer un tableau d'objets Personne . 
+       * 2.remplir le tableau avec les donnée utilisées dans exempleArray.java ( ou dans https://sharemycode.fr/mardi10)
+       * 3. affichez toute les identités des personnes ( en utilisant la méthode identite())
+       * 4. ajoutez un constructeur qui permet d'instancier 
+       * une personne en définissant directement son prenom ; 
+       * son nom et son âge . ( Cherchez sur internet ce qu'est un constructeur et comment le declarer )
+       */
+
+
+       Personne[] tableauPersonnes = new Personne[10];  // on cree un objet de tableau de  personne 
+       for (int i = 0;i<tableauPersonnes.length;i++){   // on fai une boucle pour instancier toutes les personne du tableau personne 
+            tableauPersonnes[i] = new Personne();
+       }
+
+       tableauPersonnes[0] = new Personne();           // on creer une personne dans le tableau de personne ; on instancie 
+       tableauPersonnes[0].prenom = "Gérard";
+       tableauPersonnes[0].nom = "Menfin";
+       tableauPersonnes[0].setAge(20) ; 
+
+       tableauPersonnes[1] = new Personne(); 
+       tableauPersonnes[1].prenom = "Ateur";
+       tableauPersonnes[1].nom = "Nordine";
+       tableauPersonnes[1].setAge (20) ; 
+
+       for (Personne personne : tableauPersonnes){
+        System.out.println(personne.identite() + " " + personne.getAge() );
+       }
+
+       /* NUll est un type de donnée particulier ( qu'on peut considérer comme une valeur particulière)
+        * une variable qui n'a pas été initialisée est considéré comme étant NULL .
+        */
+        
+        System.out.println(tableauPersonnes[1].identite());
+
+        
+       
+      
+
+
+
+
+    }
+}
---
+++ Personne.java
@@ -0,0 +1,59 @@
+public class Personne {
+    /* les propriétés (ou attribut) définie une classe 
+     * correspondant aux caractéristiques des objets qui vont etre creer a partir de cette classe . 
+     */
+    public String prenom = "defaut";
+    public String nom ; 
+    private int age ; 
+
+
+    /*CONSTRUCTEUR 
+     * un constructeur est une méthode quand un objet est instancié . 
+     * Toutes les classes ont un constructeur par défaut en java .
+     * le construcuteur a forcement le meme  nom que la classe . par exemple :  
+     * Personne p = new Personne();
+     * 
+     * on peut définir un nouveau constructeur tant qu'on veut tant que la signature est différente . 
+     * un constructeur ne DOIT PAS retourner une valeur . doonc il n'y a pas de type avant le nom de la méthode lors de la déclaration .
+     * en général , les constructeurs sont utilisés pour initialiser la valeur des propriétés de l'objet . 
+      
+     */
+    
+
+    /* une fonction dans une classe est appelée une METHODE  */
+
+    public Personne(String name, String firstname , int old ){
+        nom = name ;
+        prenom = firstname ; 
+        age = old ; 
+    }
+/* quand on definit un construcuteur , le construcuteur par défaut n'est plus accessible .
+ * Si on veut l'utiliser à nouveau , il faut déclarer un constructeur sans argument .
+ NB : on peut laisser les {} vides si aucune instruction ne doit être exécutée . 
+ PS : on peut déclarer autant de constructeur que l'on veut ; les arguments ou les types doivent etre différent . 
+
+ */
+
+    public Personne (String prenom, String nom){
+       this.prenom = prenom ; 
+        this.nom = nom ; 
+    }
+
+    public Personne() {}
+
+    public String identite(){
+        return this.prenom + " " + this.nom ;
+    }
+
+    public String identite(boolean vrai ){
+        return this.prenom + " " + this.nom + ",agé de " + this.age + "ans"; 
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age ;
+    }
+}
